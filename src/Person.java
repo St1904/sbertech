cded62c9b5d3cd7b4ef133afd4d1abfbd3b66c0c
Черковский Андрей Андреевич <AACherkovskiy.SBT@sberbank.ru>
2016-09-14 17:54:14 +0300

    notes

--- src/Person.java
+++ src/Person.java
@@ -8,36 +8,63 @@ public class Person {
         this.name = name;
     }
 
+    public boolean isMan() {
+        return man;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Person getSpouse() {
+        return spouse;
+    }
+
     /**
      * This method checks gender of persons. If genders are not equal - tries to marry.
      * If one of them has another spouse - execute divorce(sets spouse = null for husband and wife. Example: if both persons have spouses - then divorce will set 4 spouse to null) and then executes marry().
+     *
      * @param person - new husband/wife for this person.
      * @return - returns true if this person has another gender than passed person and they are not husband and wife, false otherwise
      */
     public boolean marry(Person person) {
-        if (this.man != person.man && !this.spouse.equals(person)) {
-            if (this.spouse != null) {
-                this.divorce();
-            }
-            if (person.spouse != null) {
-                person.divorce();
-            }
-            this.spouse = person;
-            person.spouse = this;
-            return true;
-        } else {
+        if (person == null
+                || isMan() == person.isMan()
+                || getSpouse() == person) {
             return false;
         }
+
+        divorce();
+        person.divorce();
+
+        spouse = person;
+        person.spouse = this;
+        return true;
+
+//        if (this.man != person.man && !this.spouse.equals(person)) {
+//            if (this.spouse != null) {
+//                this.divorce();
+//            }
+//            if (person.spouse != null) {
+//                person.divorce();
+//            }
+//            this.spouse = person;
+//            person.spouse = this;
+//            return true;
+//        } else {
+//            return false;
+//        }
     }
 
     /**
      * Sets spouse = null if spouse is not null
+     *
      * @return true - if person status has been changed
      */
     public boolean divorce() {
         if (spouse != null) {
             spouse.spouse = null;
-            this.spouse = null;
+            spouse = null;
             return true;
         } else {
             return false;
